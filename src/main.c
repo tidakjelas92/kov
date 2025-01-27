984ecbe261136a388571eed40ee04da69c03a9c3
hansen <hansenan14@gmail.com>
2025-01-27 13:35:43 +0700

    feat: debugging macros

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 
-// PRIVATE void draw_debug_fps(void) {
-	// char buffer[10] = { 0 };
-	// snprintf(buffer, sizeof(buffer), "%d fps", GetFPS());
-	// DrawText(buffer, 5, 5, 10, THEME_BLACK);
-// }
+#ifndef NDEBUG
+PRIVATE void draw_debug_fps(void) {
+	char buffer[10] = { 0 };
+	snprintf(buffer, sizeof(buffer), "%d fps", GetFPS());
+	DrawText(buffer, 5, 5, 10, THEME_BLACK);
+}
+#endif
 
 PRIVATE void kov_update(void) {
 	f32 delta = GetFrameTime();
@@ -17,7 +19,9 @@ PRIVATE void kov_render(void) {
 	ClearBackground(THEME_WHITE);
 
 	scene_render();
-	// draw_debug_fps();
+#ifndef NDEBUG
+	draw_debug_fps();
+#endif
 	EndDrawing();
 }
 
@@ -27,6 +31,9 @@ int main(void) {
 	InitAudioDevice();
 	SetTargetFPS(30);
 	SetExitKey(KEY_NULL);
+#ifndef NDEBUG
+	SetTraceLogLevel(LOG_DEBUG);
+#endif
 
 	resources_init();
 
